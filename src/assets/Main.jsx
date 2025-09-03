d084fbc3ea5654c66fcc4ada4c3cef480eaa3b36
NotAlex575 <dapontealessandro89gmail.com>
2025-09-03 20:42:34 +0200

    completata sezione Submit dei dati (però mi risponde solo con errore 400)

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -12,6 +12,8 @@ const Main = () =>{
 
     const [dataForm, setDataForm] = useState(dataFormStarter);
 
+
+    //INPUT
     const newDataForm = (event) => {
         const { name, value, type, checked } = event.target;
 
@@ -20,51 +22,62 @@ const Main = () =>{
        })
     }
 
+    //SUBMIT
+    const submitDataForm = (event) =>{
+        event.preventDefault();
+
+        axios.post("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts", dataForm).then((resp) =>{
+            console.log("Dati inviati con successo! ", resp.data);
+            setDataForm(dataFormStarter)
+        })
+        .catch((err) =>{
+            console.error("Dati non inviati con successo, errore: ", err)
+        })
+    }
+
     return(
         <main>
             <div className="container">
                 <div className="row">
-                    <div className="col-12">
-                        <form>
+                    <form onSubmit={submitDataForm}>
+                        <div className="col-12">
                             <label>Autore</label>
                             <input className="input-text"
                              type="text" 
                              name="author"
+                             value={dataForm.author}
                              onChange={newDataForm}/>
-                        </form>
-                    </div>
-                    <div className="col-12">
-                        <form>
+                        </div>
+                        <div className="col-12">
                             <label>Titolo</label>
                             <input className="input-text"
                              type="text" 
                              name="title"
+                             value={dataForm.title}
                              onChange={newDataForm}/>
-                        </form>
-                    </div>
-                    <div className="col-12">
-                        <form>
+                        </div>
+                        <div className="col-12">
                             <label>Testo</label>
                             <input className="input-text"
                              type="text" 
                              name="body"
+                            value={dataForm.body}
                              onChange={newDataForm}/>
-                        </form>
-                    </div>
-                    <div className="col-12">
-                        <form>
+                        </div>
+                        <div className="col-12">
                             <label>Pubblico?</label>
-                            <input className="input-text"
+                            <input className="input-checkbox"
                              type="checkbox" 
                              name="public"
+                             checked={dataForm.public}
                              onChange={newDataForm}/>
-                        </form>
-                    </div>
-                    <div className="col-12">
-                        <div className="flex-start">
-                            <button type="submit">Invia</button>
                         </div>
-                    </div>
+                        <div className="col-12">
+                            <div className="flex-start">
+                                <button type="submit">Invia</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </main>
