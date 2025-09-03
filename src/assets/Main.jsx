7b61c8127285fcf61dfdb9c34413e5cbbf8399fd
NotAlex575 <dapontealessandro89gmail.com>
2025-09-03 19:35:09 +0200

    creato lato grafico dell'esercizio (con i vari form e button)

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -1,6 +1,38 @@
 const Main = () =>{
     return(
-        <main></main>
+        <main>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <form>
+                            <label>Autore</label>
+                            <input type="text" />
+                        </form>
+                    </div>
+                    <div className="col-12">
+                        <form>
+                            <label>Titolo</label>
+                            <input type="text" />
+                        </form>
+                    </div>
+                    <div className="col-12">
+                        <form>
+                            <label>Testo</label>
+                            <input type="text" />
+                        </form>
+                    </div>
+                    <div className="col-12">
+                        <form>
+                            <label>Pubblico?</label>
+                            <input type="checkbox" />
+                        </form>
+                    </div>
+                    <div className="flex-start">
+                        <button type="submit">Invia</button>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
 
