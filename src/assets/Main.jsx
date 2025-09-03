f0079b2c517f00a6a1a0350adfdedf1b03d7b415
NotAlex575 <dapontealessandro89gmail.com>
2025-09-03 19:44:58 +0200

    aggiornate parti grafice dei label, button e i 2 tipi di input

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -6,29 +6,31 @@ const Main = () =>{
                     <div className="col-12">
                         <form>
                             <label>Autore</label>
-                            <input type="text" />
+                            <input className="input-text" type="text" />
                         </form>
                     </div>
                     <div className="col-12">
                         <form>
                             <label>Titolo</label>
-                            <input type="text" />
+                            <input className="input-text" type="text" />
                         </form>
                     </div>
                     <div className="col-12">
                         <form>
                             <label>Testo</label>
-                            <input type="text" />
+                            <input className="input-text" type="text" />
                         </form>
                     </div>
                     <div className="col-12">
                         <form>
                             <label>Pubblico?</label>
-                            <input type="checkbox" />
+                            <input className="input-checkbox" type="checkbox" />
                         </form>
                     </div>
-                    <div className="flex-start">
-                        <button type="submit">Invia</button>
+                    <div className="col-12">
+                        <div className="flex-start">
+                            <button type="submit">Invia</button>
+                        </div>
                     </div>
                 </div>
             </div>
