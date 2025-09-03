ded5efb099c1a5e4d5332285fa559e9270b5958d
NotAlex575 <dapontealessandro89gmail.com>
2025-09-03 20:05:33 +0200

    creata costante per la gestione degli input text e checkbox

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -11,7 +11,14 @@ const Main = () =>{
     };
 
     const [dataForm, setDataForm] = useState(dataFormStarter);
-    
+
+    const newDataForm = (event) => {
+        const { name, value, type, checked } = event.target;
+
+       setDataForm({
+        ...dataForm, [name]: type === "checkbox" ? checked : value,
+       })
+    }
 
     return(
         <main>
@@ -20,25 +27,37 @@ const Main = () =>{
                     <div className="col-12">
                         <form>
                             <label>Autore</label>
-                            <input className="input-text" type="text" />
+                            <input className="input-text"
+                             type="text" 
+                             name="author"
+                             onChange={newDataForm}/>
                         </form>
                     </div>
                     <div className="col-12">
                         <form>
                             <label>Titolo</label>
-                            <input className="input-text" type="text" />
+                            <input className="input-text"
+                             type="text" 
+                             name="title"
+                             onChange={newDataForm}/>
                         </form>
                     </div>
                     <div className="col-12">
                         <form>
                             <label>Testo</label>
-                            <input className="input-text" type="text" />
+                            <input className="input-text"
+                             type="text" 
+                             name="body"
+                             onChange={newDataForm}/>
                         </form>
                     </div>
                     <div className="col-12">
                         <form>
                             <label>Pubblico?</label>
-                            <input className="input-checkbox" type="checkbox" />
+                            <input className="input-text"
+                             type="checkbox" 
+                             name="public"
+                             onChange={newDataForm}/>
                         </form>
                     </div>
                     <div className="col-12">
